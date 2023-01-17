b75b6d2bf1379199717b593c2636b2b3338bb726
Daniel22L <dlamasnovoa@gmail.com>
2023-01-17 08:29:32 +0100

    update license

--- classes/Voxels.m
+++ classes/Voxels.m
@@ -6,8 +6,7 @@ segmentation of point clouds of large metallic truss bridges with modelling
 purposes.
 The program is free software: you can redistribute it and/or modify it 
 under the terms of the GNU General Public License as published by the Free
-Software Foundation, either version 3 of the License, or (at your option) 
-any later version.
+Software Foundation, either version 3 of the License, or any later version.
 The program is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
 FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
