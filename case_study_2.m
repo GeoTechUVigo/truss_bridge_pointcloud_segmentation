301b05a8c741b878b3290899e34acdb27665dd52
Daniel22L <dlamasnovoa@gmail.com>
2023-01-17 07:52:12 +0100

    update license

--- case_study_2.m
+++ case_study_2.m
@@ -1,3 +1,22 @@
+%{
+Copyright (C) 2023 GeoTECH Group geotech@uvigo.gal
+Copyright (C) 2023 Daniel Lamas Novoa daniel.lamas.novoa@uvigo.gal
+Copyright (C) 2023 Andrés Justo Domínguez andres.justo.dominguez@uvigo.gal
+This file is part of the program Automated instance and semantic 
+segmentation of point clouds of large metallic truss bridges with modelling
+purposes.
+The program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) 
+any later version.
+The program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+more details.
+You should have received a copy of the GNU General Public License along 
+with the program in COPYING. If not, see <https://www.gnu.org/licenses/>. 
+%}
+
 clear all; clc; 
 
 path_in='C:\Datos\Traballo\Codes\datos_publicar\case_study_2.las';
--- classes/pointCloud_.m
+++ classes/pointCloud_.m
@@ -1,6 +1,20 @@
-% Copyright (C) 2023 GeoTECH Group geotech@uvigo.gal
-% Under the terms of the GNU General Public License.
-% See the LICENCE.md and COPYING.md file for more details.
+%{
+Copyright (C) 2023 GeoTECH Group geotech@uvigo.gal
+This file is part of the program Automated instance and semantic 
+segmentation of point clouds of large metallic truss bridges with modelling
+purposes.
+The program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) 
+any later version.
+The program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+more details.
+You should have received a copy of the GNU General Public License along 
+with the program in COPYING. If not, see <https://www.gnu.org/licenses/>. 
+%}
+
 
 classdef pointCloud_ < matlab.mixin.Copyable & vision.internal.EnforceScalarHandle
 %Clase modificada a partir de la clase pointCloud_ de matlab. Se añaden las
